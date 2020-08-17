9e913c9aa84f83fbdecfa6e3eac2e01cc4ecbc7f
cpustejovsky <charles.pustejovsky@gmail.com>
2020-08-17 17:19:26 -0400

    adding users param to sendEmail function

--- mailer.go
+++ mailer.go
@@ -10,24 +10,32 @@ import (
 	"github.com/mailgun/mailgun-go/v4"
 )
 
-func sendEmail() {
+func sendEmail(u []User) {
 	// Create an instance of the Mailgun Client
 	err := godotenv.Load()
 	mg, err := mailgun.NewMailgunFromEnv()
 	if err != nil {
 		fmt.Println(err)
 	}
-	sender := "sender@estuaryapp.com"
-	subject := "Fancy subject!"
-	body := "Hello from Mailgun Go!"
+	sender := "twitter-updates@estuaryapp.com"
+	subject := "Twitter Updates"
+	body := "" //use template
 	recipient := "charles.pustejovsky@gmail.com"
 
+	// Create a new template
+
 	// The message object allows you to add attachments and Bcc recipients
-	message := mg.NewMessage(sender, subject, body, recipient)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
-	// Send the message with a 10 second timeout
-	resp, id, err := mg.Send(ctx, message)
+
+	m := mg.NewMessage(sender, subject, body, recipient)
+	m.SetTemplate("my-template")
+
+	// Add the variables to be used by the template
+	m.AddVariable("title", "Testing How Users Look")
+	m.AddVariable("body", fmt.Sprintf("<ul><li>%v</li><li>%v</li></ul>", u[0].name, u[1].name))
+
+	resp, id, err := mg.Send(ctx, m)
 
 	if err != nil {
 		log.Fatal(err)
