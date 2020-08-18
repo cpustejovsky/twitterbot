7cd82696adca7563affbac35e9ff60cbec79a50f
cpustejovsky <charles.pustejovsky@gmail.com>
2020-08-18 13:31:11 -0400

    split up functions in mailer.go

--- mailer.go
+++ mailer.go
@@ -11,19 +11,19 @@ import (
 	"github.com/mailgun/mailgun-go/v4"
 )
 
-func sendEmail(u []User) {
+func checkUsers(u []User) {
 	empties := 0
 	for _, user := range u {
-		fmt.Println(len(user.tweets))
 		if len(user.tweets) == 0 {
 			empties++
 		}
 	}
-	fmt.Println(len(u))
 	if len(u) == empties {
 		log.Fatal("no users to send email to.")
 	}
+}
 
+func setUpMailGun() *mailgun.MailgunImpl {
 	// Create an instance of the Mailgun Client
 	enverr := godotenv.Load()
 	if enverr != nil {
@@ -31,32 +31,41 @@ func sendEmail(u []User) {
 	}
 	mg, err := mailgun.NewMailgunFromEnv()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
+	return mg
+}
+
+func formatHtml(u []User, m *mailgun.Message) {
+	//TODO: make use of html/templates for templating
+	var tweets bytes.Buffer
+	tweets.WriteString("<h1>Daily Tweet Update</h1>")
+	for _, user := range u {
+		tweets.WriteString("<h3>Tweets from " + user.name + "</h3><ul>")
+		if len(user.tweets) == 0 {
+			tweets.WriteString("<li>No new tweets.</li>")
+		}
+		for _, tweet := range user.tweets {
+			tweets.WriteString("<li>" + tweet.text + " <a target='_blank' rel='noopener noreferrer' href=" + tweet.link + ">(link)</a></li>")
+		}
+		tweets.WriteString("</ul>")
+	}
+
+	m.SetHtml(tweets.String())
+}
+
+func sendEmail(u []User) {
+	checkUsers(u)
+	mg := setUpMailGun()
+
 	sender := "twitter-updates@estuaryapp.com"
 	subject := "Twitter Updates"
 	html := ""
 	recipient := "charles.pustejovsky@gmail.com"
 
 	m := mg.NewMessage(sender, subject, html, recipient)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//TODO: make use of html/templates for templating
-	var tweets bytes.Buffer
-	tweets.WriteString("<h1>Daily Tweet Update</h1>")
-	for _, user := range u {
-		tweets.WriteString("<h3>Tweets from" + user.name + "</h3><ul>")
-		if len(user.tweets) == 0 {
-			tweets.WriteString("<li>No new tweets.</li>")
-		}
-		for _, tweet := range user.tweets {
-			tweets.WriteString("<li>" + tweet.text + "\n<a target='_blank' rel='noopener noreferrer' href=" + tweet.link + ">(link)</a></li>")
-		}
-		tweets.WriteString("</ul>")
-	}
 
-	m.SetHtml(tweets.String())
+	formatHtml(u, m)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
