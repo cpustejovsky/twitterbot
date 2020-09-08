8f84beb373f0dd65c427fa36f6f7694f14258338
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-08 18:30:19 -0400

    add test for Greek function

---
+++ twitter_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestGreek(t *testing.T) {
+
+	t.Run("returns false for no Greek in text", func(t *testing.T) {
+		englishTxt := "It was 65 years ago, at night, when the pogrom against the Greek community of Istanbul started. We remember those whose very lives were lost, and their way of life was destroyed by a preplanned act of terror built on a lie."
+		got := greek(englishTxt)
+		want := false
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("returns true for Greek in text", func(t *testing.T) {
+		greekTxt := "Πριν από 65 χρόνια, τη νύχτα της 6ης Σεπτεμβρίου, ξεκινούσε το πογκρόμ της Ελληνικής κοινότητας της Κωνσταντινούπολης. Σήμερα θυμόμαστε αυτούς που χάθηκαν, εκείνες και εκείνους που η ζωή τους καταστράφηκε από μια προμελετημένη τρομοκρατία, που στήθηκε πάνω σε ένα ψέμα."
+		got := greek(greekTxt)
+		want := true
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
