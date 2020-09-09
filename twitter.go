c271feabcf0ffdfc3fab84f03e25a7fdaa562166
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-09 15:47:06 -0400

    splitting up functions

--- twitter.go
+++ twitter.go
@@ -29,6 +29,32 @@ type User struct {
 	tweets []UserTweet
 }
 
+type TwitterBot struct {
+	client *twitter.Client
+}
+
+func (tb *TwitterBot) newBot() error {
+	creds := loadCreds()
+	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
+	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+
+	_, _, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	tb.client = client
+	return nil
+}
+
 func greek(tweet string) bool {
 	for _, char := range tweet {
 		if char >= 945 && char <= 1023 {
@@ -53,34 +79,13 @@ func loadCreds() Credentials {
 	return creds
 }
 
-func GetClient() (*twitter.Client, error) {
-	creds := loadCreds()
-	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
-	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
-
-	httpClient := config.Client(oauth1.NoContext, token)
-	client := twitter.NewClient(httpClient)
-
-	verifyParams := &twitter.AccountVerifyParams{
-		SkipStatus:   twitter.Bool(true),
-		IncludeEmail: twitter.Bool(true),
-	}
-
-	_, _, err := client.Accounts.VerifyCredentials(verifyParams)
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
-}
-
-func FindUserTweets(client *twitter.Client, userName string, c chan User) {
+func (tb *TwitterBot) FindUserTweets(userName string, c chan User) {
 	params := &twitter.UserTimelineParams{
 		ScreenName: userName,
 		Count:      5,
 		TweetMode:  "extended",
 	}
-	tweets, resp, err := client.Timelines.UserTimeline(params)
+	tweets, resp, err := tb.client.Timelines.UserTimeline(params)
 	u := User{
 		name: userName,
 	}
@@ -89,23 +94,34 @@ func FindUserTweets(client *twitter.Client, userName string, c chan User) {
 		fmt.Println(err)
 		c <- u
 	}
-	u.name = tweets[0].User.Name
+	u = tb.ModifyAndAddTweetsToUser(u, tweets)
+	c <- u
+}
+
+func (tb *TwitterBot) ModifyAndAddTweetsToUser(u User, tweets []twitter.Tweet) User {
 	for _, tweet := range tweets {
+		prevLiked := true
 		greek := greek(tweet.FullText)
 		if greek == false {
 			if tweet.Favorited == false {
-				var p twitter.FavoriteCreateParams
-				p.ID = tweet.ID
-				client.Favorites.Create(&p)
-				ut := UserTweet{
-					text: tweet.FullText,
-					id:   tweet.IDStr,
-					link: fmt.Sprintf("https://twitter.com/%v/status/%v", userName, tweet.IDStr),
-				}
-				u.tweets = append(u.tweets, ut)
+				tb.LikeTweet(tweet)
+				prevLiked = true
 			}
+			ut := UserTweet{
+				text:  tweet.FullText,
+				id:    tweet.IDStr,
+				link:  fmt.Sprintf("https://twitter.com/%v/status/%v", u.name, tweet.IDStr),
+				liked: prevLiked,
+			}
+			u.tweets = append(u.tweets, ut)
 		}
 	}
-	fmt.Printf("found %v unliked tweets from %v\n", len(u.tweets), u.name)
-	c <- u
+	u.name = tweets[0].User.Name
+	return u
+}
+
+func (tb *TwitterBot) LikeTweet(tweet twitter.Tweet) {
+	var p twitter.FavoriteCreateParams
+	p.ID = tweet.ID
+	tb.client.Favorites.Create(&p)
 }
