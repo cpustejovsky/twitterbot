ccc1ceccdcb86bcf10a92bbc905389934bf0b54a
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-16 18:57:12 -0400

    removing todos that have been resolved

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,8 +24,6 @@ type application struct {
 }
 
 func main() {
-	//TODO: move all fatal logs to main function or at least the handlers?
-	//TODO: log all errors to a /tmp/error.log file and all info to a /tmp/info.log
 	if os.Getenv("PORT") == "" {
 		if err := godotenv.Load(); err != nil {
 			log.Fatal(err)
