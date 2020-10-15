e87fc37f2a2c28364223d2a860cd29225acb4936
cpustejovsky <charles.pustejovsky@gmail.com>
2020-10-15 13:54:41 -0400

    fixing test

--- mailer_test.go
+++ mailer_test.go
@@ -6,16 +6,12 @@ import (
 
 func TestEmptyError(t *testing.T) {
 	t.Run("Ensures EmptyError is of error type", func(t *testing.T) {
-
 		var _ error = &EmptyError{}
-
 	})
 
 	t.Run("returns same string value when implemented", func(t *testing.T) {
-
-		want := "no users to send email to."
+		want := "No new tweets for queried users."
 		got := (*EmptyError).Error(&EmptyError{})
-
 		assertEqualStrings(t, got, want)
 	})
 }
